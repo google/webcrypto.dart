8b5e478d9c243b106f91916bbcf07eb5834fba8b
Jonas Finnemann Jensen <jonasfj@google.com>
2019-02-04 20:47:03 +0100

    Added test case for importSpkiKey with fixed public key

--- test/webcrypto/rsassapkcs1v15_test.dart
+++ test/webcrypto/rsassapkcs1v15_test.dart
@@ -12,11 +12,47 @@ Stream<List<int>> asStream(String data) async* {
 
 const _sampleMessage = 'Good morning is an important message!';
 
+/// Sample public key for verifying [_sampleSignature] of [_sampleMessage].
+final _samplePublicKey = base64.decode(
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjImMAbh2BuIjd+N/sSoUJ1wHHuddQa'
+    'Lvf2tPk7oMLIFv24lxbSHgE4N5ighdrlBjrbTXikZn6PZzsrWh6XJyHX0Kful/vihRiCOGpinL'
+    'HmAkr7/NLfTmqHgAP0XjyX+woARFIftwHM4h0h1bd6gR/qlvFCd03TR6a/0uoir3vpoXcI/AB7'
+    'LVZh63vjGgBTpDQATlxgTaqyVhaZOiAQzJYhphAYTJKb7c9RjzU12KqeaEQsJR+20Iol5oGhCX'
+    'wsWhsR8ue8MaE2sxsmECUO3rdyRysY8gJrcbJo+y/XkH/AGoTvmGQ2JOnL/vaiEjubzVPsrlRd'
+    'm++pjVIjZJVM1mMwIDAQAB');
+
+/// Sample signature of [_sampleMessage] to be verified by [_samplePublicKey].
+final _sampleSignature = base64.decode(
+    'QBkNEUq6BJH/RPECAPpCKvJZxHaUN22K33CirEgWeX7QFNIjHFyEcQ0mtTrO7RxY8qs98PGpnF'
+    'OjzyLL+03BwlasEptImy3HkQ3SY3l070DJjKVAvB139+FfLKEjvoXVh0Y6HmONj+A+CARbJpAe'
+    '7dMNUAjsbJ/7Jrl0L8JmPyXQmIjSEYPjIEpDZy0iq8ZhWNX2jUtcEULQhEY1+c+RGlSRBHpZbz'
+    'URIKyRzzX3LWYklweCDoCNGg6UYxBajPg677QyfD2yrulLlDrXA8gVz4YBMHYJDRhJ9Z67wxfI'
+    'OE530wYYL9FzWsm5A57UQ8Xu0Z+8NACwwukDpjizZZHW2w==');
+
 void main() {
   // Shared state
   RsassaPkcs1V15PrivateKey privateKey;
   RsassaPkcs1V15PublicKey publicKey;
   List<int> sig;
+
+  test('importSpkiKey', () async {
+    final key = await RsassaPkcs1V15PublicKey.importSpkiKey(
+      keyData: _samplePublicKey,
+      hash: HashAlgorithm.sha256,
+      extractable: true,
+      usages: [KeyUsage.verify],
+    );
+    // Compare two exports of the key
+    final keyData = await key.exportSpkiKey();
+    expect(base64.encode(keyData), equals(base64.encode(_samplePublicKey)));
+
+    // Test verify
+    final result = await key.verify(
+      signature: _sampleSignature,
+      data: asStream(_sampleMessage),
+    );
+    expect(result, isTrue);
+  });
 
   test('generateKey', () async {
     final pair = await RsassaPkcs1V15PrivateKey.generateKey(
@@ -107,7 +143,7 @@ void main() {
     expect(base64.encode(keyData2), equals(base64.encode(keyData2)));
 
     // Test verify
-    final result = await publicKey.verify(
+    final result = await key.verify(
       signature: sig,
       data: asStream(_sampleMessage),
     );
