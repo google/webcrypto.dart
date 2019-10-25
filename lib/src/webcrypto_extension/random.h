af94c5483bf29aa5f2be5c443f5b39fdae56061d
Jonas Finnemann Jensen <jonasfj@google.com>
2019-10-25 18:44:37 +0200

    Removed all the FFI and extension related stuff, to focus on browser-only support

--- lib/src/webcrypto_extension/random.h
+++
@@ -1,9 +0,0 @@
-#ifndef WEBCRYPTO_EXTENSION_RANDOM_H
-#define WEBCRYPTO_EXTENSION_RANDOM_H
-
-#include <include/dart_api.h>
-#include <include/dart_native_api.h>
-
-void getRandomValues(Dart_NativeArguments arguments);
-
-#endif /* WEBCRYPTO_EXTENSION_RANDOM_H */
--- lib/src/webcrypto_extension/rsassa.h
+++
@@ -1,9 +0,0 @@
-#ifndef WEBCRYPTO_EXTENSION_RSASSA_H
-#define WEBCRYPTO_EXTENSION_RSASSA_H
-
-#include <include/dart_api.h>
-#include <include/dart_native_api.h>
-
-// EVP_PKEY
-
-#endif /* WEBCRYPTO_EXTENSION_RSASSA_H */
