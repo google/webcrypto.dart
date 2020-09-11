96d3bdd3ecba889def4187ebec37a1f90e33e7c5
Jonas Finnemann Jensen <jonasfj@google.com>
2020-09-12 01:32:13 +0200

    Removed conditional dart:cli usage

--- lib/src/boringssl/lookup/utils.dart
+++ lib/src/boringssl/lookup/utils.dart
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:io' show Platform, File;
-import 'finddotdarttool.dart';
+import 'dart:io' show Platform, Directory, File;
 import 'dart:ffi';
 import 'symbols.generated.dart';
 
@@ -39,7 +38,7 @@ String get libraryFileName {
 ///
 /// Returns `null` if it could not be found.
 Pointer<Void> Function(Sym) lookupLibraryInDotDartTool() {
-  final dotDartTool = findDotDartTool();
+  final dotDartTool = _findDotDartTool();
   if (dotDartTool == null) {
     return null;
   }
@@ -61,5 +60,35 @@ Pointer<Void> Function(Sym) lookupLibraryInDotDartTool() {
     // Return a function from Sym to lookup using `webcrypto_lookup_symbol`
     return (Sym s) => webcrypto_lookup_symbol(s.index);
   }
+  return null;
+}
+
+/// Find the `.dart_tool/` folder, returns `null` if unable to find it.
+Uri _findDotDartTool() {
+  // HACK: Because 'dart:isolate' is unavailable in Flutter we have no means
+  //       by which we can find the location of the package_config.json file.
+  //       Which we need, because the binary library created by:
+  //         flutter pub run webcrypto:setup
+  //       is located relative to this path. As a workaround we use
+  //       `Platform.script` and traverse level-up until we find a
+  //       `.dart_tool/package_config.json` file.
+
+  // Find script directory
+  Uri root;
+  if (Platform.script.isScheme('data')) {
+    // If `Platform.script` is a data: [Uri] then we are being called from
+    // `package:test`, luckily this means that CWD is project root.
+    root = Directory.current.uri;
+  } else {
+    root = Platform.script.resolve('./');
+  }
+
+  // Traverse up until we see a `.dart_tool/package_config.json` file.
+  do {
+    if (File.fromUri(root.resolve('.dart_tool/package_config.json'))
+        .existsSync()) {
+      return root.resolve('.dart_tool/');
+    }
+  } while (root != (root = root.resolve('..')));
   return null;
 }
