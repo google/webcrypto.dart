1059235650e04c0fae900ccdf8ebf3294d0291cf
Georg Wechslberger <tauu@h2overclock.de>
2021-01-09 04:05:41 +0100

    fixed import path of example app

--- example/test_driver/app.dart
+++ example/test_driver/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/driver_extension.dart';
-import '../lib/main.dart' as app;
+import 'package:webcrypto_example/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
