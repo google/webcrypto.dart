93a4b6befac69b34d94b8dbe91b8c60f4cab9783
Jonas Finnemann Jensen <jonasfj@google.com>
2021-01-13 18:24:51 +0100

    Added with-chromedriver.sh tool

---
+++ tool/with-chromedriver.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT="$DIR/.."
+
+TARGET="$ROOT/.dart_tool/chromedriver"
+
+# Detect chrome version
+CHROME_VERSION=$(google-chrome --version | grep -Po '(\d+.\d+.\d+)')
+echo "CHROME_VERSION: $CHROME_VERSION"
+
+# Create target file
+mkdir -p "$TARGET"
+touch "$TARGET/CHROME_VERSION";
+
+# Check that chrome haven't been updated
+if [[ $(cat "$TARGET/CHROME_VERSION") != "$CHROME_VERSION" ]]; then
+  # Clear cache
+  rm -rf "$TARGET"
+  mkdir -p "$TARGET"
+
+  # Find matching chromedriver version
+  BASE_URL='https://chromedriver.storage.googleapis.com'
+  CHROMEDRIVER_VERSION=$(curl -L -s "$BASE_URL/LATEST_RELEASE_$CHROME_VERSION")
+  echo "CHROMEDRIVER_VERSION: $CHROMEDRIVER_VERSION"
+
+  # Detect platform
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    PLATFORM='linux64'
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    PLATFORM='mac64'
+  else
+    echo "Unsupported OSTYPE: $OSTYPE"
+    exit 1
+  fi
+
+  # Download chromedriver
+  curl -L "$BASE_URL/$CHROMEDRIVER_VERSION/chromedriver_$PLATFORM.zip" \
+    -o "$TARGET/chromedriver.zip"
+
+  # Extract to target
+  unzip "$TARGET/chromedriver.zip" -d "$TARGET"
+
+  # Store CHROME_VERSION so we can easily check if it's outdated
+  echo "$CHROME_VERSION" > "$TARGET/CHROME_VERSION"
+fi
+
+# Start chromedriver and kill it when we exit
+"$TARGET/chromedriver" --port=4444 &
+CHROMEDRIVER_PID="$!"
+trap "kill $CHROMEDRIVER_PID; wait $CHROMEDRIVER_PID 2>/dev/null;" EXIT
+
+# Give chromedriver time to start
+sleep 1
+
+# Run whatever command we were told to run
+$@
