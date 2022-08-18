184fe6dc11c00f53b4f8a5813bf6c143429eee3f
Jonas Finnemann Jensen <jonasfj@google.com>
2022-08-18 12:46:13 +0200

    Minor tweaks for lints

--- bin/setup.dart
+++ bin/setup.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 import 'dart:convert';
 
