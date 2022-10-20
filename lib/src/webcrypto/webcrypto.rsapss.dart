ed6a49358a8d32ca0c86a75c398410f43fc3bd51
Jonas Finnemann Jensen <jonasfj@google.com>
2022-10-20 15:00:18 +0200

    Better docs

--- lib/src/webcrypto/webcrypto.rsapss.dart
+++ lib/src/webcrypto/webcrypto.rsapss.dart
@@ -14,6 +14,56 @@
 
 part of webcrypto;
 
+/// RSASSA-PSS private key for signing messages.
+///
+/// An [RsaPssPrivateKey] instance holds a private RSA key for computing
+/// signatures using the RSASSA-PSS scheme as specified in [RFC 3447][1].
+///
+/// An [RsaPssPrivateKey] can be imported from:
+///  * [PKCS #8][2] format using [RsaPssPrivateKey.importPkcs8Key], and,
+///  * [JWK][3] format using [RsaPssPrivateKey.importJsonWebKey].
+///
+/// A public-private [KeyPair] consisting of a [RsaPssPublicKey] and a
+/// [RsaPssPrivateKey] can be generated using [RsaPssPrivateKey.generateKey].
+///
+/// {@template RSASSA-PSS-Example:generate-sign-verify}
+/// **Example**
+/// ```dart
+/// import 'dart:convert' show utf8;
+/// import 'package:webcrypto/webcrypto.dart';
+///
+/// // Generate a key-pair.
+/// final keyPair = await RsaPssPrivateKey.generateKey(
+///   4096,
+///   BigInt.from(65537),
+///   Hash.sha256,
+/// );
+///
+/// // Use the same saltLength for signing and verifying
+/// const saltLength = 256 / 8;
+///
+/// // Using privateKey Bob can sign a message for Alice.
+/// final message = 'Hi Alice';
+/// final signature = await keyPair.privateKey.signBytes(
+///   utf8.encode(message),
+///   saltLength,
+/// );
+///
+/// // Given publicKey and signature Alice can verify the message from Bob.
+/// final isValid = await keypair.publicKey.verifyBytes(
+///   signature,
+///   utf8.encode(message),
+///   saltLength,
+/// );
+/// if (isValid) {
+///   print('Authentic message from Bob: $message');
+/// }
+/// ```
+/// {@endtemplate}
+///
+/// [1]: https://tools.ietf.org/html/rfc3447
+/// [2]: https://tools.ietf.org/html/rfc5208
+/// [3]: https://tools.ietf.org/html/rfc7517
 @sealed
 abstract class RsaPssPrivateKey {
   RsaPssPrivateKey._(); // keep the constructor private.
