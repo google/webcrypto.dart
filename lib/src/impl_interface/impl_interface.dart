d0845a80558ad81f14a51a2cb91e95ef0481abda
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2024-07-27 01:06:22 +0530

    Update lib/src/impl_interface/impl_interface.dart
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>

--- lib/src/impl_interface/impl_interface.dart
+++ lib/src/impl_interface/impl_interface.dart
@@ -6,6 +6,22 @@ import 'dart:async';
 
 part 'impl_interface.aescbc.dart';
 
+/// Interface to be provided by platform implementations.
+///
+/// A platform implementation of `package:webcrypto` must define a
+/// constant `webCryptImpl` as follows:
+/// ```dart
+/// const WebCryptoImpl webCryptImpl = const _MyPlatformImplemetation();
+/// ```
+///
+/// The only platform implementations are:
+///  * `lib/src/impl_ffi/impl_ffi.dart`,
+///  * `lib/src/impl_js/impl_js.dart`, and,
+///  * `lib/src/impl_stub/impl_stub.dart`.
+///
+/// These interfaces are not public and should not be implemented
+/// outside this package. Should platform implementations ever become
+/// plugable these interfaces will be renamed.
 abstract interface class WebCryptoImpl {
   StaticAesCbcSecretKeyImpl get aesCbcSecretKey;
 }
