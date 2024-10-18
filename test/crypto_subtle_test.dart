aba5be2bb4b4d7ebc5fce6049f39000e3bb9683e
Jonas Finnemann Jensen <jonasfj@google.com>
2024-10-18 13:02:23 +0200

    Fix lints and check lints in CI

--- test/crypto_subtle_test.dart
+++ test/crypto_subtle_test.dart
@@ -1,4 +1,3 @@
-@TestOn('browser')
 // Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+@TestOn('browser')
+library;
 
 import 'dart:js_interop';
 import 'dart:typed_data';
