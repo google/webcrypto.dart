c75430cf0346c7efb5f2bdf9d7e527dd7138c5ca
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-02-10 22:38:30 +0900

    More support for wasm (#178)
    * fix: Using return values (wasm support)
    
    * test: Update getRandomValue test and support wasm error
    
    * feat: Use js_interop instead of html (wasm support)
    
    * feat: Add wasm ci test and set timeout-minutes
    
    * test: Stop separating jobs by strategy
    
    * fix: getRandomValues
    
    * feat: Unify the behavior of dart2js and dart2wasm
    
    * fix: Simplify
    
    * feat: Create UnknownError class
    
    * test: fix
    
    * refactor: Simplify UnknownError
    
    * refactor: Added processing branching by kIsWasm
    
    * fix: Show only kIsWasm
    
    * remove: Remove kIsWasm
    
    * Update lib/src/crypto_subtle.dart
    
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>
    
    * fix: Use jsArray and dartArray
    
    * fix: minor fix
    
    * test: Remove skip option
    
    * test: Add tests for various lists
    
    * fix: Remove kIsWasm
    
    * fix: throw ArgumentError when type is not supported
    
    * chore: Check wasm coverage
    
    * chore: Remove unused step
    
    * chore: Remove wasm option from windows
    
    ---------
    
    Co-authored-by: Jonas Finnemann Jensen <jopsen@gmail.com>

--- lib/src/crypto_subtle.dart
+++ lib/src/crypto_subtle.dart
@@ -333,24 +333,62 @@ extension type JSRsaOtherPrimesInfo(JSObject _) implements JSObject {
 }
 
 TypedData getRandomValues(TypedData array) {
+  // The `.toJS` on `Uint8List` (and friends) may:
+  //   * cast,
+  //   * create a wrapper, or,
+  //   * clone.
+  // See: https://api.dart.dev/dart-js_interop/Uint8ListToJSUint8Array/toJS.html
+  //
+  // Thus, when we do `.toJS` and pass the resulting object to `getRandomValues`,
+  // we don't know if `.toJS` simply cast or created a wrapper such that changes
+  // made by `getRandomBytes` are reflected in `array`.
+  //
+  // For this reason, we must use `.setAll` to copy the values into `array`, if it
+  // was not cast.
+  //
+  // See also: https://github.com/dart-lang/sdk/issues/59651
   if (array is Uint8List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else if (array is Uint16List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else if (array is Uint32List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else if (array is Int8List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else if (array is Int16List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else if (array is Int32List) {
-    window.crypto.getRandomValues(array.toJS);
-    return array;
+    final jsArray = array.toJS;
+    window.crypto.getRandomValues(jsArray);
+    final dartArray = jsArray.toDart;
+    if (array != dartArray) {
+      array.setAll(0, dartArray);
+    }
   } else {
     throw ArgumentError.value(
       array,
@@ -358,6 +396,8 @@ TypedData getRandomValues(TypedData array) {
       'Unsupported TypedData type',
     );
   }
+
+  return array;
 }
 
 Future<ByteBuffer> decrypt(
