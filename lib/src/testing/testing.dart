aa51bc9cbd8a44e60ed684649b534c3727c202ee
Harshita Yadav <seemayadavanuj123@gmail.com>
2026-09-03 20:16:20 +0530

    fix(aes-gcm): validate browser tag lengths (#385)

--- lib/src/testing/testing.dart
+++ lib/src/testing/testing.dart
@@ -30,6 +30,8 @@ import 'webcrypto/rsassapkcs1v15.dart' as rsassapkcs1v15;
 // Other test files, that don't use TestRunner
 import 'webcrypto/random.dart' as random;
 import 'webcrypto/digest.dart' as digest;
+import 'regression/aes_gcm_invalid_tag_length.dart'
+    as aes_gcm_invalid_tag_length;
 import 'regression/derive_bits_zero_length.dart' as derive_bits_zero_length;
 import 'regression/issue_60_trailing_bytes.dart' as issue_60_trailing_bytes;
 import 'regression/jwk_base64url.dart' as jwk_base64url;
@@ -63,6 +65,7 @@ void runAllTests(
     for (final r in _testRunners) ...r.tests(),
     ...random.tests(),
     ...digest.tests(),
+    ...aes_gcm_invalid_tag_length.tests(),
     ...issue_60_trailing_bytes.tests(),
     ...jwk_base64url.tests(),
     ...derive_bits_zero_length.tests(),
